673ba6e39f5b433c3062ccf1b8e2228e321e0bee
Emanuel Santos Souza de Jesus <emanuelsantoss42@gmail.com>
2023-04-14 20:02:26 -0300

    Avaliação do Back-end

---
+++ src/app/models/Support.ts
@@ -0,0 +1,4 @@
+export class Support {
+    url: string | undefined;
+    text: string | undefined;
+}
---
+++ src/app/services/reqres.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReqresService {
+
+  url: string = 'https://reqres.in/api/users'
+
+  constructor(private http: HttpClient) { 
+    this.getAll();
+  }
+
+
+  getAll(){
+    return this.http.get(this.url).subscribe(data => {
+      data = data;
+    });
+  }
+
+  getOne(id:number){
+    return this.http.get(`${this.url}/${id}`);
+  }
+
+  create(user: User){
+    return this.http.post(this.url, user);
+  }
+
+  update(user: User){
+    return this.http.put(`${this.url}/ ${user.id}`, user);
+  }
+
+  delete(id: number){
+    return this.http.delete(`${this.url}/${id}`);
+  }
+}
